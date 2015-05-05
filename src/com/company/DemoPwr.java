10c3719ca958fc2d7e90ce74059175ab9c1af84f
AndyTrump <zfmacpro@gmail.com>
2015-05-05 23:33:01 +0800

    Good

--- src/com/company/DemoPwr.java
+++ src/com/company/DemoPwr.java
@@ -9,16 +9,16 @@ class Pwr{
     double val;
 
     Pwr(double base,int exp){
-        b=base;
-        e=exp;
+        this.b=base;
+        this.e=exp;
 
-        val=1;
+        this.val=1;
         if(exp==0) return;
             for (;exp>0;exp--)
-                val=val*base;
+                this.val=this.val*base;
         }
         double get_pwr(){
-            return val;
+            return this.val;
         }
 }
 public class DemoPwr {
